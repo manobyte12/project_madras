0b4597fbede45729e74ecfb01c240092b1a66a9d
manobyte12 <113885236+manobyte12@users.noreply.github.com>
2024-03-08 09:42:35 +0530

    Create first_file01.java
    codes added

---
+++ first_file01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.System;
+
+public class first_file01
+  {
+    public static void main(String []args)
+    {
+      System.out.println("hello world");
+    }
+  }
+
